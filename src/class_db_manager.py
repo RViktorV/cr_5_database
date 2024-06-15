53a735b4060a478f4ad0c1ba014eef9bd3037413
vitek <rgavih@yandex.ru>
2024-06-15 17:42:36 +0700

    changes to the file class_db_manager.py.py

--- src/class_db_manager.py
+++ src/class_db_manager.py
@@ -2,6 +2,7 @@ import psycopg2
 from config import config
 from tabulate import tabulate
 
+
 class DBManager:
     """
     Класс для работы с базой данных PostgreSQL, содержащей информацию о работодателях и вакансиях.
@@ -40,17 +41,31 @@ class DBManager:
         """)
         return self.cur.fetchall()
 
+    def get_vacancies_by_company_id(self, company_id):
+        """
+        Получает список вакансий по ID компании.
+        :param company_id: ID компании.
+        :return: Список кортежей (название компании, название вакансии, зарплата от, зарплата до, ссылка на вакансию).
+        """
+        self.cur.execute("""
+            SELECT e.name, v.name, v.salary_from, v.salary_to, v.url
+            FROM vacancies v
+            JOIN employers e ON v.employer_id = e.id
+            WHERE v.employer_id = %s
+        """, (str(company_id),))
+        return self.cur.fetchall()
+
     def get_avg_salary(self):
         """
         Получает среднюю зарплату по вакансиям.
         :return: Средняя зарплата.
         """
         self.cur.execute("""
-            SELECT AVG((salary_from + salary_to) / 2)
+            SELECT ROUND(AVG((salary_from + salary_to) / 2))
             FROM vacancies
             WHERE salary_from IS NOT NULL AND salary_to IS NOT NULL
         """)
-        return self.cur.fetchone()[0]
+        return int(self.cur.fetchone()[0])
 
     def get_vacancies_with_higher_salary(self):
         """
@@ -80,7 +95,6 @@ class DBManager:
         """, (f'%{keyword}%',))
         return self.cur.fetchall()
 
-
     def print_select(self, query):
         """
         Метод выводит результаты запроса в виде таблицы
@@ -95,7 +109,6 @@ class DBManager:
         table = tabulate(results, headers=colnames, tablefmt="grid")
         return '\n' + table
 
-
     def print_companies_and_vacancies_count(self):
         """
         Метод выводит компании и количество вакансий в виде таблицы.
@@ -106,10 +119,9 @@ class DBManager:
         table = tabulate(results, headers=colnames, tablefmt="fancy_grid")
         return table
 
-
     def print_all_vacancies(self):
         """
-        Метод выводит св виден таблицы писок всех вакансий с указанием названия компании,
+        Метод выводит в виден таблицы писок всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию.
         """
         results = self.get_all_vacancies()
@@ -117,7 +129,6 @@ class DBManager:
 
         table = tabulate(results, headers=colnames, tablefmt="fancy_grid")
         return table
-
 
     def print_vacancies_with_higher_salary(self):
         """
@@ -129,12 +140,23 @@ class DBManager:
         table = tabulate(results, headers=colnames, tablefmt="fancy_grid")
         return table
 
-    def print_vacancies_with_keyword(self):
+    def print_vacancies_with_keyword(self, keyword):
         """
         Метод выводит в виден таблицы вакансий, в названии которых содержатся переданные в метод ключевое слово
         """
-        results = self.get_vacancies_with_keyword('developer')
+        results = self.get_vacancies_with_keyword(keyword)
         colnames = ['Company Name', 'Job Title', 'salary from', 'salary up to', 'URL']
+
+        table = tabulate(results, headers=colnames, tablefmt="fancy_grid")
+        return table
+
+    def print_vacancies_by_company_id(self, company_id):
+        """
+        Метод выводит в виде таблицы вакансии указанной компании по ее ID.
+        :param company_id: ID компании.
+        """
+        results = self.get_vacancies_by_company_id(company_id)
+        colnames = ['Company Name', 'Job Title', 'Salary From', 'Salary To', 'URL']
 
         table = tabulate(results, headers=colnames, tablefmt="fancy_grid")
         return table
@@ -145,4 +167,3 @@ class DBManager:
         """
         self.cur.close()
         self.conn.close()
-
